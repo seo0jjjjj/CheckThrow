db16cc97d8339259dadfcd974e900bdc26faec05
seo0j <seo0jjjjj@gmail.com>
2022-08-24 14:32:08 +0900

    Initial commit

---
+++ src/main/java/me/rang/checkthrow/checkthrow/CheckThrow.java
@@ -0,0 +1,17 @@
+package me.rang.checkthrow.checkthrow;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CheckThrow extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
