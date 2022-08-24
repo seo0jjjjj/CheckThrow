ce3c50715ace25de660954b270218de81ff0fe27
lnx_dx <lnx_dx@outlook.kr>
2022-08-24 16:31:17 +0900

    으악

--- src/main/java/me/rang/checkthrow/checkthrow/CheckThrowListener.java
+++ src/main/java/me/rang/checkthrow/checkthrow/CheckThrowListener.java
@@ -1,32 +1,41 @@
 package me.rang.checkthrow.checkthrow;
 
+import org.bukkit.NamespacedKey;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.persistence.PersistentDataContainer;
+import org.bukkit.persistence.PersistentDataType;
 
 /// 이벤트 핸들러
 public class CheckThrowListener implements Listener {
 
     private final CheckThrow pl;
 
-    private final FileConfiguration config;
-
     private boolean isCancelled = false;
+    private ItemStack is = null;
 
-    public CheckThrowListener(CheckThrow pl) { this.pl = pl; this.config = pl.getConfig(); }
+    public CheckThrowListener(CheckThrow pl) { this.pl = pl; }
 
     @EventHandler
     public void onPlayerDropItem(PlayerDropItemEvent event) {
         Player p = event.getPlayer();
-        if (config.contains(p.getName()) && config.getBoolean(p.getName())) { // config 에 플레이어의 이름이 포함되어 있고, 콘피그에 저장된 플레이어 이름의 값이 true일 때
-            if (!isCancelled) { // 취소된 적이 없을 때
+        PersistentDataContainer data = p.getPersistentDataContainer();
+        String str = data.get(new NamespacedKey(pl, "isQtoggled"), PersistentDataType.STRING);
+
+        if (str != null && str.equals(String.valueOf(true))) { // config 에 플레이어의 이름이 포함 && 콘피그에 저장된 플레이어 이름의 값이 true 일 때
+            if (!isCancelled && event.getItemDrop().getItemStack().equals(is)) { // 취소된 적이 없을 때 && 기존의 아이템과 드랍하려는 아이템이 같을 때
                 event.setCancelled(true);
+                is = event.getItemDrop().getItemStack();
                 isCancelled = true;
                 p.sendMessage("아이템을 버리려면 다시 한번 아이템을 버리는 키를 누르길 바랍니다.");
             } else {
                 isCancelled = false;
+                is = null;
             }
         }
 
