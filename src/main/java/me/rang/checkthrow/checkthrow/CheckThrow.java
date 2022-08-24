553c4484704b989fa179793e40995cb49734f701
lnx_dx <lnx_dx@outlook.kr>
2022-08-24 14:47:51 +0900

    헉 안녕하세요

--- src/main/java/me/rang/checkthrow/checkthrow/CheckThrow.java
+++ src/main/java/me/rang/checkthrow/checkthrow/CheckThrow.java
@@ -8,7 +8,7 @@ public final class CheckThrow extends JavaPlugin {
     public void onEnable() {
         // Plugin startup logic
         System.out.println("hello World!"); //seo0jjjjj
-        System.out.println("헉 안녕하세요");
+        System.out.println("헉 안녕하세요이얏호");
 
     }
 
